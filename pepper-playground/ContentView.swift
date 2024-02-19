419ab0f0b30495d1d8e080e916d04bef8c66c82e
Isha Zhang <ishz@uchicago.edu>
2024-02-19 17:26:59 -0600

    Initial Commit

---
+++ pepper-playground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pepper-playground
+//
+//  Created by Zhang on 2024/2/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pepper-playground/pepper_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  pepper_playgroundApp.swift
+//  pepper-playground
+//
+//  Created by Zhang on 2024/2/19.
+//
+
+import SwiftUI
+
+@main
+struct pepper_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
